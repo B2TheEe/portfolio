70f20d773d2a9cc8697ab16f3cfbf9909b11b167
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-10-20 14:21:03 +0200

    Add AboutMe model

--- portfolio/aboutMe/admin.py
+++ portfolio/aboutMe/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SingletonModel,AboutMe
 
-# Register your models here.
+admin.site.register(SingletonModel)
+admin.site.register(AboutMe)
--- portfolio/aboutMe/models.py
+++ portfolio/aboutMe/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from phonenumber_field.formfields import PhoneNumberField
+
 
 # Create your models here.
+class SingletonModel(models.Model):
+    _singleton = models.BooleanField(default=True, editable=False, unique=True)
+
+    class Meta:
+        abstract = True
+
+
+class AboutMe(SingletonModel):
+    photo = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    phone_number = PhoneNumberField(blank=True)
+    email_address = models.EmailField()
+    github = models.CharField(max_length=250)
+    linkedIn = models.CharField(max_length=250)
+    about_me = models.TextField(max_length=1500)
