087d156bd027e7c3b30f370e2f619f9b287b9f43
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-10-23 18:42:05 +0200

    WorkExperiences in template

--- portfolio/worke/views.py
+++ portfolio/worke/views.py
@@ -3,11 +3,6 @@ from .models import WorkExperience
 # Create your views here.
 def get_all_workexperience(request):
     workexperiences = WorkExperience.objects.all()
-    print(workexperiences[0].name)
-    print(workexperiences[0].description)
-    print(workexperiences[1].name)
-    print(workexperiences[1].description)
-
     context = {
         "workexperiences": workexperiences,
     }
