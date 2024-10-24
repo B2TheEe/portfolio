c9a43f44d1e6ad118f9c73623f10539770ff1197
Bente Schopman <schopmanbenteb2theee@gmail.com>
2024-10-24 10:08:58 +0200

    Finishing up WorkExperience

--- portfolio/worke/views.py
+++ portfolio/worke/views.py
@@ -1,8 +1,9 @@
+from django.db.models.functions import Coalesce
 from django.shortcuts import render
 from .models import WorkExperience
 # Create your views here.
 def get_all_workexperience(request):
-    workexperiences = WorkExperience.objects.all()
+    workexperiences = WorkExperience.objects.all().order_by("from_date")
     context = {
         "workexperiences": workexperiences,
     }
