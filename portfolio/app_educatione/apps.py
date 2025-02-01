344e554688adbe1d1dd6476ccb626937f0deaa48
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-01 17:10:10 +0100

    Staticfiles,media,locale

--- portfolio/app_educatione/apps.py
+++ portfolio/app_educatione/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class EducationeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_educatione'
+    verbose_name = _('app_educatione')
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django
+
+django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,24 +35,28 @@ STATIC_ROOT = 'staticfiles'
 
 INSTALLED_APPS = [
     "modeltranslation",
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # My Apps
     "app_aboutMe",
     "app_worke",
     "app_educatione",
     "app_portfolio",
     "app_skills",
-
+    'rosetta',
+    # Default Apps
+    'django.contrib.admin',
+    "admin",
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -66,9 +73,10 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                #'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -110,11 +118,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
+from django.utils.translation import gettext_lazy as _
 
 LANGUAGES = {
-    ('en', "English"),
-    ("nl", "Dutch"),
+    ('en', _('English')),
+    ("nl", _("Dutch")),
 }
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale/',
+]
 
 TIME_ZONE = 'UTC'
 
