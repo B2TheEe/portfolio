2b2f766efbe545f8bd6b9dc4678ceef284a28ca8
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-04 10:15:07 +0100

    Unwanted deletion

--- portfolio/app_aboutMe/views.py
+++ portfolio/app_aboutMe/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.translation.template import context_re
+from django.utils.translation import gettext as _
 from .models import AboutMe
 
 
 # Create your views here.
+
 def about_me_view(request):
     about_me_data = AboutMe.objects.get(pk=1)
     data = {
-        "test": "test",
-        "about_me": about_me_data,
+        "about_me": _(about_me_data),
     }
     return render(request, template_name="about-me.html",context=data)
