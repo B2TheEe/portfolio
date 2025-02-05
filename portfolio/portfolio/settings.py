f8756bdeae4c4f825999790527b10a20ab8a219c
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-05 14:05:54 +0100

    Revert "requirements and locale edited"
    This reverts commit 1dac465570ae2e66bc1bf2fb9a5a91f82a6632a5.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rosetta',
     "app_aboutMe",
     "app_worke",
     "app_educatione",
