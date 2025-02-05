5a345351a9388946917474a1c88a0d69424f01be
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-05 13:57:38 +0100

    oRevert "locale and templates"
    This reverts commit a292f64cdf46b8aa2783080d350ec27a6731b922, reversing
    changes made to 3e233bd4bc89fbd1a40ac0a528dddc0b3be0b388.

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,17 +16,14 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
-from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rosetta/', include('rosetta.urls')),
     path('', include(('app_aboutMe.urls', 'app_aboutMe'), namespace='app_aboutMe')),
     #path("", include("app_aboutMe.urls")),
     #path("work", include("app_worke.urls", 'work'),namespace="work"),
@@ -40,15 +37,13 @@ urlpatterns = [
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns = i18n_patterns(
-    path(_('admin/'), admin.site.urls),
-    path('rosetta/', include('rosetta.urls')),
-    path("nl/overmij", include('app_aboutMe.urls',namespace='app_aboutMe')),
-    path('nl/vaardigheden', include('app_skills.urls', namespace='app_skills')),
+i18n_patterns = [
+    #path("nl/overmij", include('app_aboutMe.urls',namespace='app_aboutMe')),
+    #path('nl/vaardigheden', include('app_skills.urls', namespace='app_skills')),
     path('nl/werk', include('app_worke.urls')),
-     path('nl/portfolio', include('aportfolio.urls', namespace='portfolio')),
-)
-
+   # path('nl/portfolio', include('aportfolio.urls', namespace='portfolio')),
+   # path('nl/admin', admin.site.urls),
+]
 urlpatterns += i18n_patterns
 
 #
