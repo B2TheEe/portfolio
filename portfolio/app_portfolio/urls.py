bc84086d3823e16e10401ecd20fd5f0f5e4fd059
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-07 22:48:38 +0100

    Portolio-item added

--- portfolio/app_portfolio/urls.py
+++ portfolio/app_portfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.get_all_portfolio_items, name="index"),
+    path("/<pk>", views.get_portfolio_item, name="portfolio-item")
 ]
--- portfolio/app_portfolio/views.py
+++ portfolio/app_portfolio/views.py
@@ -7,3 +7,10 @@ def get_all_portfolio_items(request):
         "portfolioitems": portfolioitems,
     }
     return render(request,template_name="portfolio.html",context=context)
+
+def get_portfolio_item(request, pk):
+    portfolio_item = PortfolioItem.objects.get(pk=pk)
+    context = {
+        "portfolioitem": portfolio_item
+    }
+    return render(request,template_name="portfolio-item.html", context=context)
