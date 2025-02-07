0aa48bf48dd883e3dbcabaf70fcb47d39bd9cb23
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-07 19:55:56 +0100

    App_portfolio modified

--- portfolio/app_portfolio/models.py
+++ portfolio/app_portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PortfolioItem(models.Model):
+    photo = models.ImageField(upload_to='media/')
+    gitHubUrl = models.CharField(max_length=100,default=None)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+
+
+
+
+    def __str__(self):
+        return self.name
--- portfolio/app_portfolio/views.py
+++ portfolio/app_portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import PortfolioItem
 # Create your views here.
+def get_all_portfolio_items(request):
+    portfolioitems = PortfolioItem.objects.all()
+    context = {
+        "portfolioitems": portfolioitems,
+    }
+    return render(request,template_name="education.html",context=context)
