93ab40b73926638457a82feff75b7ca28d001139
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-02-08 11:40:05 +0100

    admin,urls,views @ app_portfolio

--- portfolio/app_portfolio/views.py
+++ portfolio/app_portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    data = {
+        "test": 'test',
+    }
+    return render(request, "portfolio.html",context=data)
