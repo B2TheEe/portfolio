b3feb5beaeee487bfcdc89ca8fb7d6f9d56999d9
Bente Schopman <schopmanbenteb2theee@gmail.com>
2025-04-11 11:06:46 +0200

    Improvements

--- portfolio/app_landingpage/views.py
+++ portfolio/app_landingpage/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 from django.http import FileResponse
+from django.conf import settings
 from .models import LandingPage
 
 # Create your views here.
@@ -16,5 +17,12 @@ def index(request):
         raise Http404("No Landingpage matches the given query.")
 
 
-def download_cv(reauest):
-        return FileResponse(open('media/documents/CV_BenteSchopman_EN_v0125_GSkfl1A.pdf', 'rb'), as_attachment=True, content_type='application/pdf')
+def download_cv(request):
+    lang = settings.LANGUAGE_CODE
+
+    if lang == 'en-us':
+      return FileResponse(open('media/documents/CV_BenteSchopman_EN_v0125_8ZoYu50.pdf', 'rb'), as_attachment=True, content_type='application/pdf')
+    else:
+      return FileResponse(open('media/documents/CV_BenteSchopman_NL_v0125_5RzvHIB.pdf', 'rb'), as_attachment=True,
+                            content_type='application/pdf')
+
